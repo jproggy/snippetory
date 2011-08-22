1a851fd8a0b613b40fc090c329551f11ec036dab
rotn <rotn@154fa62a-ae8f-480d-91fd-dc75d68c4636>
2011-08-22 19:35:41 +0000

    Added some comment, changed a number of trancoding rules
    git-svn-id: https://svn.code.sf.net/p/snippetory/code/trunk@17 154fa62a-ae8f-480d-91fd-dc75d68c4636

--- src/main/java/de/jproggy/snippetory/Encodings.java
+++ src/main/java/de/jproggy/snippetory/Encodings.java
@@ -8,7 +8,18 @@ import de.jproggy.snippetory.impl.SnippetoryException;
 import de.jproggy.snippetory.spi.Encoding;
 
 public enum Encodings implements Encoding {
-
+	/**
+	 * It's assumed that Snippetory is used in a modern Unicode based environment. Though,
+	 * only a minimal escaping is done: 
+	 * <table width="150">
+	 * <tr><td>&lt;</td><td>--></td><td>&amp;lt;</td></tr>
+	 * <tr><td>&amp;</td><td>--></td><td>&ampamp;</td></tr>
+	 * </table>
+	 * <br />
+	 * As xml is a compound format, i.e. it can contain other formats, almost each other 
+	 * will be placed within without any transcoding. Only on plain text the normal escaping
+	 * is applied.
+	 */
 	xml {
 		@Override
 		public void escape(StringBuilder target, String val) {
@@ -25,13 +36,21 @@ public enum Encodings implements Encoding {
 		}
 		@Override
 		public void transcode(StringBuilder target, String value, String encodingName) {
-			if (html.name().equals(encodingName)) {
-				target.append(value);
-			} else {
+			if (plain.name().equals(encodingName)) {
 				escape(target, value);
+			} else {
+				target.append(value);
 			}
 		}
 	},
+	
+	/**
+	 * html is derived from xml. It just converts line breaks to &lt;br />-tags to enable
+	 * transporting of simple formatting within the data bound. Be aware: this applies 
+	 * to data bound, not to some kind of source code like in html pages, so we do not 
+	 * break with the good practice of separating the layout of source code and it's 
+	 * resulting appearance.
+	 */
 	html {
 		@Override
 		public void escape(StringBuilder target, String val) {
@@ -56,13 +75,16 @@ public enum Encodings implements Encoding {
 		}
 		@Override
 		public void transcode(StringBuilder target, String value, String encodingName) {
-			if (xml.name().equals(encodingName)) {
-				target.append(value);
-			} else {
+			if (plain.name().equals(encodingName)) {
 				escape(target, value);
+			} else {
+				target.append(value);
 			}
 		}
 	},
+	/**
+	 * Applies url encoding to the data
+	 */
 	url {
 		@Override
 		public void escape(StringBuilder target, String val) {
@@ -73,6 +95,10 @@ public enum Encodings implements Encoding {
 			}
 		}
 	},
+	/**
+	 * Most C-based languages have almost the same rules. This fits at least Java and
+	 * JavaScript.
+	 */
 	string {
 		@Override
 		public void escape(StringBuilder target, String val) {
@@ -117,21 +143,23 @@ public enum Encodings implements Encoding {
 		}
 		@Override
 		public void transcode(StringBuilder target, String value, String encodingName) {
-			if (html_string.name().equals(encodingName)) {
+			if (html_string.name().equals(encodingName) || NULL.name().equals(encodingName)) {
 				target.append(value);
 			} else {
 				escape(target, value);
 			}
 		}
 	},
+	/**
+	 * In JavaScript I've sometimes data that is transported in a string before it's 
+	 * displayed as html.
+	 */
 	html_string {
 		@Override
 		public void escape(StringBuilder target, String val) {
-			try {
-				target.append(URLEncoder.encode(val, "utf-8"));
-			} catch (UnsupportedEncodingException e) {
-				throw new SnippetoryException(e);
-			}
+			StringBuilder tmp = new StringBuilder();
+			string.escape(tmp, val);
+			html.escape(target, tmp.toString());
 		}
 		@Override
 		public void transcode(StringBuilder target, String value, String encodingName) {
@@ -139,7 +167,9 @@ public enum Encodings implements Encoding {
 				string.escape(target, value);
 			}
 			else if (string.name().equals(encodingName)) {
-				html.escape(target, value);
+				// I don't expect this to have practical use. But it adds additional risk
+				// so breaking seems right.
+				throw new IncompatibleEncodingException("Can't check if content might be html");
 			} 
 			else {
 				super.transcode(target, value, encodingName);
@@ -147,12 +177,20 @@ public enum Encodings implements Encoding {
 			
 		}
 	},
+	/**
+	 * Plain text. You will get an IllegalEncodingExcption if you try to bind encoded data 
+	 * to it.
+	 */
 	plain {
 		@Override
 		public void escape(StringBuilder target, String val) {
 			target.append(val);
 		}
 	},
+	/**
+	 * The wild card encoding. Fits to any other, any other fits to this. Sometimes it's 
+	 * necessary to work around the checks. But in general you should not use it.
+	 */
 	NULL {
 		@Override
 		public void escape(StringBuilder target, String val) {
@@ -167,7 +205,10 @@ public enum Encodings implements Encoding {
 
 	@Override
 	public void transcode(StringBuilder target, String value, String encodingName) {
-		if (NULL.equals(encodingName)) {
+		if (NULL.name().equals(encodingName)) {
+			target.append(value);
+		}
+		if (plain.name().equals(encodingName)) {
 			escape(target, value);
 		}
 		throw new IncompatibleEncodingException("can't convert encoding "
