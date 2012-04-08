f44cf4b37634ccc84734349ca720e6d50b46f6d9
rotn <rotn@154fa62a-ae8f-480d-91fd-dc75d68c4636>
2012-04-08 20:08:45 +0000

    added some performance testing
    git-svn-id: https://svn.code.sf.net/p/snippetory/code/trunk@71 154fa62a-ae8f-480d-91fd-dc75d68c4636

--- src/test/java/org/jproggy/snippetory/test/RegionTest.java
+++ src/test/java/org/jproggy/snippetory/test/RegionTest.java
@@ -2,13 +2,19 @@ package org.jproggy.snippetory.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
+import org.jproggy.snippetory.Repo;
+import org.jproggy.snippetory.Template;
 import org.jproggy.snippetory.engine.Location;
 import org.jproggy.snippetory.engine.Region;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class RegionTest {
@@ -33,5 +39,70 @@ public class RegionTest {
 		region = new Region(placeHolder, parts, Collections.EMPTY_MAP);
 		assertEquals('y', region.charAt(4));
 		assertEquals('y', region.charAt(15));
+	}
+	private Template template;
+	private String[] variants = { "row1", "row2", "row3", };
+	private List<Object> data = new ArrayList<Object>();
+
+	@Before
+	public void init() {
+		template = Repo.readResource("testTable.htm").locale(Locale.US).parse();
+		for (int i = 0; i < 111; i++) {
+			data.add(String.valueOf(i));
+		}
+	}
+
+	@Test
+	public void test100() {
+		testN(100);
+	}
+
+	@Test
+	public void test1000() {
+		testN(1000);
+	}
+
+	@Test
+	public void test100_1000() {
+		for (int i = 0; i < 1000; i++) {
+			template.clear();
+			testN(100);
+		}
+	}
+
+	@Test
+	public void test10000() {
+		testN(10000);
+	}
+
+	@Test
+	public void test100000() {
+		testN(65000);
+	}
+
+	@Test
+	@Ignore
+	public void test1000000() {
+		testN(1000000);
+	}
+
+	public void testN(int n) {
+		int i = 0; 
+		try {
+			int count = 0;
+			for (; i < n; i++) {
+				Template row = template.get(variants[i % variants.length]);
+				for (int x = 0; x < 10; x++) {
+					row.get("x").set("val", data.get(count)).render();
+					count++;
+					if (count >= data.size()) count = 0;
+				}
+				row.render("row1");
+			}
+			@SuppressWarnings("unused")
+			CharSequence x = template.toString();
+		} finally {
+			Assert.assertEquals(n, i);
+		}
 	}
 }
