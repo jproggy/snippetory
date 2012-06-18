6003977c7adffeadc43d60f412d5a7dbd244e090
rotn <rotn@154fa62a-ae8f-480d-91fd-dc75d68c4636>
2012-06-18 20:04:10 +0000

    more consistent white space handling
    git-svn-id: https://svn.code.sf.net/p/snippetory/code/trunk@83 154fa62a-ae8f-480d-91fd-dc75d68c4636

--- src/main/java/org/jproggy/snippetory/engine/spi/CComments.java
+++ src/main/java/org/jproggy/snippetory/engine/spi/CComments.java
@@ -20,6 +20,28 @@ import java.util.regex.Pattern;
 import org.jproggy.snippetory.engine.RegExSyntax;
 import org.jproggy.snippetory.engine.Token.TokenType;
 
+/**
+ * C_COMMENTS syntax uses C comment areas and C++ line comments to hide template syntax 
+ * from compilers and interpreters that might be used to validate the templates.
+ * <br />
+ * The template syntax is based on a leading $ sign and braces to mark the area 
+ * of impact. On regions the name is repeated at the end, while pure locations
+ * simply close the curly bracket.
+ * <br />
+ * A special variant of the syntax allows mocking:
+ * <br />
+ * <code style="color:darkblue;">
+ * /*${name attrib="value"&#42;/<b>mock</b>/*}&#42;/;
+ * </code>
+ * <br /> 
+ * The mock will be ignored and will not be written to the output. This supports
+ * to keep the template valid for execution or compilation as for the validation
+ * environment the mock is visible and substitutes data to be bound.
+ * <br />
+ * Line breaks are supported whitespace in C_COMMENTS but not within the mock.
+ * 
+ * @author B. Ebertz
+ */
 public class CComments   extends RegExSyntax {
 
 	@Override
@@ -27,40 +49,40 @@ public class CComments   extends RegExSyntax {
 		Map<Pattern, TokenType> patterns = new LinkedHashMap<Pattern, TokenType>();
 
 		patterns.put(SYNTAX_SELECTOR, TokenType.Syntax);
-		String cbs = "\\/\\*[ \t]*";
-		String cbe = "[ \\t]*\\*\\/";
+		String cbs = "\\/\\*\\s*";
+		String cbe = "\\s*\\*\\/";
 
-		String pre = cbs + "\\$\\{[ \t]*";
-		String suff = "[ \\t]*\\}" + cbe;
+		String pre = cbs + "\\$\\{\\s*";
+		String suff = "\\s*\\}" + cbe;
 
 		Pattern mock_default = Pattern.compile(
-				pre +"(" + NAME + "(?:[ \t]*" + ATTRIBUTE + ")*)" + cbe + "(.*)" + cbs + suff);
+				pre +"(" + NAME + "(?:\\s+" + ATTRIBUTE + ")*)" + cbe + "((?:(?!/\\*).)*)" + cbs + suff, Pattern.MULTILINE);
 		patterns.put(mock_default, TokenType.Field);
 
 		Pattern start = Pattern.compile(
-				LINE_START +  "\\/\\/[ \t]*\\$\\{[ \t]*(" + NAME + 
-				"(?:[ \t]*" + ATTRIBUTE + ")*)" + LINE_END, Pattern.MULTILINE);
+				LINE_START +  "\\/\\/\\s*\\$\\{[\\s/]*(" + NAME + 
+				"(?:\\s[\\s/]*" + ATTRIBUTE + ")*)" + LINE_END, Pattern.MULTILINE);
 		patterns.put(start, TokenType.BlockStart);
 
 		start = Pattern.compile(
-				pre + "(" + NAME + "(?:[ \t]*" + ATTRIBUTE + ")*)" + cbe);
+				pre + "(" + NAME + "(?:\\s+" + ATTRIBUTE + ")*)" + cbe, Pattern.MULTILINE);
 		patterns.put(start, TokenType.BlockStart);
 
 		Pattern end = Pattern.compile(
-				LINE_START + "\\/\\/[ \t]*(" + NAME + ")[ \t]*\\}" + LINE_END, Pattern.MULTILINE);
+				LINE_START + "\\/\\/\\s*(" + NAME + ")\\s*\\}" + LINE_END, Pattern.MULTILINE);
 		patterns.put(end, TokenType.BlockEnd);
 
-		end = Pattern.compile(cbs + "(" + NAME + ")[ \t]*\\}" + cbe);
+		end = Pattern.compile(cbs + "(" + NAME + ")\\s*\\}" + cbe, Pattern.MULTILINE);
 		patterns.put(end, TokenType.BlockEnd);
 		
 		Pattern comment = Pattern.compile(
 				LINE_START + "\\/\\/\\/.*" +  LINE_END, Pattern.MULTILINE);
 		patterns.put(comment, TokenType.Comment);
 		
-		Pattern field = Pattern.compile(pre + "(" + NAME + "(?:[ \t]*" + ATTRIBUTE + ")*)" + suff);
+		Pattern field = Pattern.compile(pre + "(" + NAME + "(?:\\s+" + ATTRIBUTE + ")*)" + suff, Pattern.MULTILINE);
 		patterns.put(field, TokenType.Field);
 
-		Pattern nameless = Pattern.compile(pre + "(" + ATTRIBUTE + "(?:[ \t]*" + ATTRIBUTE + ")*)" + suff);
+		Pattern nameless = Pattern.compile(pre + "(" + ATTRIBUTE + "(?:\\s+" + ATTRIBUTE + ")*)" + suff, Pattern.MULTILINE);
 		patterns.put(nameless, TokenType.Field);
 		return new RegexParser(data, patterns);
 	}
