5e776e53db0258584bb18aec46e3ee52203e6798
rotn <rotn@154fa62a-ae8f-480d-91fd-dc75d68c4636>
2012-10-14 19:44:07 +0000

    Utilities for handling encoded data
    git-svn-id: https://svn.code.sf.net/p/snippetory/code/trunk@119 154fa62a-ae8f-480d-91fd-dc75d68c4636

---
+++ src/main/java/org/jproggy/snippetory/spi/CharDataSupport.java
@@ -0,0 +1,47 @@
+package org.jproggy.snippetory.spi;
+
+import org.jproggy.snippetory.Encodings;
+import org.jproggy.snippetory.engine.SnippetoryException;
+import org.jproggy.snippetory.engine.chars.EncodedContainer;
+
+public class CharDataSupport {
+	public static boolean isCharData(Object value) {
+		return value instanceof EncodedData || value instanceof CharSequence;
+	}
+	
+	public static CharSequence toCharSequence(Object chars) {
+		if (chars instanceof CharSequence) {
+			return (CharSequence)chars;
+		}
+		return ((EncodedData)chars).toCharSequence();
+	}
+	
+	public static String getEncoding(Object chars) {
+		if (chars instanceof EncodedData) {
+			((EncodedData)chars).getEncoding();
+		}
+		return Encodings.plain.getName();
+	}
+	
+	public static EncodedData toEncodedData(Object chars) {
+		if (chars instanceof EncodedData) {
+			return (EncodedData)chars;
+		}
+		if (chars instanceof CharSequence) {
+			return new EncodedContainer((CharSequence)chars, Encodings.plain.getName());
+		}
+		throw new SnippetoryException("No character data: " + chars);
+	}
+	
+	public static EncodedData toEncodedData(Object chars, String encoding) {
+		if (chars instanceof EncodedData) {
+			EncodedData data = (EncodedData)chars;
+			if (getEncoding(chars).equals(encoding)) return data;
+			return new EncodedContainer(data.toCharSequence(), encoding);
+		}
+		if (chars instanceof CharSequence) {
+			return new EncodedContainer((CharSequence)chars, encoding);
+		}
+		throw new SnippetoryException("No character data: " + chars);
+	}
+}
