3d5ab4d76b946496cc0124010bc5204dad69a0f8
rotn <rotn@154fa62a-ae8f-480d-91fd-dc75d68c4636>
2014-11-17 20:13:00 +0000

    removed unused methods
    git-svn-id: https://svn.code.sf.net/p/snippetory/code/trunk@168 154fa62a-ae8f-480d-91fd-dc75d68c4636

--- snippetory-core/src/main/java/org/jproggy/snippetory/spi/CharDataSupport.java
+++ snippetory-core/src/main/java/org/jproggy/snippetory/spi/CharDataSupport.java
@@ -1,8 +1,6 @@
 package org.jproggy.snippetory.spi;
 
 import org.jproggy.snippetory.Encodings;
-import org.jproggy.snippetory.engine.SnippetoryException;
-import org.jproggy.snippetory.engine.chars.EncodedContainer;
 
 /**
  * The Snippetory templating solution works with two different form of character data:
@@ -58,36 +56,5 @@ public class CharDataSupport {
 			((EncodedData)chars).getEncoding();
 		}
 		return Encodings.plain.getName();
-	}
-
-	/**
-	 * Converts chars to an EncodedData object.CharSequences are considered plain text.
-	 * This method may only be called with character data.
-	 */
-	public static EncodedData toEncodedData(Object chars) {
-		if (chars instanceof EncodedData) {
-			return (EncodedData)chars;
-		}
-		if (chars instanceof CharSequence) {
-			return new EncodedContainer((CharSequence)chars, Encodings.plain.getName());
-		}
-		throw new SnippetoryException("No character data: " + chars);
-	}
-
-	/**
-	 * Converts data to an EncodedData object. Provided encoding is used for
-	 * CharSequences only.
-	 * This method may only be called with character data.
-	 */
-	public static EncodedData toEncodedData(Object chars, String encoding) {
-		if (chars instanceof EncodedData) {
-			EncodedData data = (EncodedData)chars;
-			if (getEncoding(chars).equals(encoding)) return data;
-			return new EncodedContainer(data.toCharSequence(), encoding);
-		}
-		if (chars instanceof CharSequence) {
-			return new EncodedContainer((CharSequence)chars, encoding);
-		}
-		throw new SnippetoryException("No character data: " + chars);
 	}
 }
