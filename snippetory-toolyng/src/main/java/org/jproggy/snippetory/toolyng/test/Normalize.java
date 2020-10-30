e7d1e1082b86bb269553219fe0846c999d98c3b7
rotn <rotn@154fa62a-ae8f-480d-91fd-dc75d68c4636>
2020-10-30 16:07:55 +0000

    fix modifier order
    git-svn-id: https://svn.code.sf.net/p/snippetory/code/trunk@228 154fa62a-ae8f-480d-91fd-dc75d68c4636

--- snippetory-toolyng/src/main/java/org/jproggy/snippetory/toolyng/test/Normalize.java
+++ snippetory-toolyng/src/main/java/org/jproggy/snippetory/toolyng/test/Normalize.java
@@ -1,18 +1,19 @@
 package org.jproggy.snippetory.toolyng.test;
 
-import java.util.regex.Pattern;
-
 import junit.framework.ComparisonFailure;
+
+import java.util.regex.Pattern;
 
 public class Normalize {
   private static final Pattern WS = Pattern.compile("[ \t\r\n]*");
 
-  static public void assertNormEquals(String message, String expected, String actual) {
+  public static void assertNormEquals(String message, String expected, String actual) {
     if (!normalize(expected).equals(normalize(actual))) {
-      String cleanMessage= message == null ? "" : message;
+      String cleanMessage = message == null ? "" : message;
       throw new ComparisonFailure(cleanMessage, expected, actual);
     }
   }
+
   public static void assertNormEquals(String expected, String actual) {
     assertNormEquals(null, expected, actual);
   }
