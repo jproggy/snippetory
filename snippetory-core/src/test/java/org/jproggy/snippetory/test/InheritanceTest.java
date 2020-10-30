3d53144fd2a65354021cf55a77860869f59d067d
rotn <rotn@154fa62a-ae8f-480d-91fd-dc75d68c4636>
2020-10-30 16:10:28 +0000

    Use hamcrest
    git-svn-id: https://svn.code.sf.net/p/snippetory/code/trunk@231 154fa62a-ae8f-480d-91fd-dc75d68c4636

--- snippetory-core/src/test/java/org/jproggy/snippetory/test/InheritanceTest.java
+++ snippetory-core/src/test/java/org/jproggy/snippetory/test/InheritanceTest.java
@@ -14,8 +14,9 @@
 
 package org.jproggy.snippetory.test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.jproggy.snippetory.Syntaxes.XML_ALIKE;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Date;
 import java.util.Locale;
@@ -23,7 +24,7 @@ import java.util.TimeZone;
 
 import org.jproggy.snippetory.Template;
 import org.jproggy.snippetory.TemplateContext;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class InheritanceTest {
@@ -35,8 +36,8 @@ class InheritanceTest {
   private static final java.sql.Date D1 = java.sql.Date.valueOf("2011-10-15");
   private static final Date D1_TIME = new Date(D1.getTime() + 3_915_000L);
 
-  @DisplayName("Inheritance.FromRootTest")
-  static class FromRoot {
+  @Nested
+  class FromRoot {
 
     @Test
     void dateToRegion() {
@@ -44,32 +45,28 @@ class InheritanceTest {
       Template date = ctx.parse("<t:test>Date: {v:d1}<t:> Other date: {v:d2}</t:>\n</t:test>");
       date.get("test").set("d1", D1).set("d2", D1_TIME).render();
       date.get("test").set("d1", D2).render();
-      assertEquals(
-              "Date: Oct 15, 2011, 12:00:00 AM GMT Other date: Oct 15, 2011, 1:05:15 AM GMT\nDate: Oct 6, 2011, 12:00:00 AM GMT\n",
-              date.toString()
-      );
+      assertThat(date.toString(), is(
+              "Date: Oct 15, 2011, 12:00:00 AM GMT Other date: Oct 15, 2011, 1:05:15 AM GMT\nDate: Oct 6, 2011, 12:00:00 AM GMT\n"
+      ));
       Template test = date.get("test");
-      assertEquals("Date: {v:d1}\n", test.toString());
+      assertThat(test.toString(), is("Date: {v:d1}\n"));
       test.set("d2", D2).set("d1", D2);
-      assertEquals(
-              "Date: Oct 6, 2011, 12:00:00 AM GMT Other date: Oct 6, 2011, 12:00:00 AM GMT\n",
-              test.toString()
-      );
-      assertEquals(
-              "Date: Oct 15, 2011, 12:00:00 AM GMT Other date: Oct 15, 2011, 1:05:15 AM GMT\nDate: Oct 6, 2011, 12:00:00 AM GMT\n",
-              date.toString()
-      );
+      assertThat(test.toString(), is(
+              "Date: Oct 6, 2011, 12:00:00 AM GMT Other date: Oct 6, 2011, 12:00:00 AM GMT\n"
+      ));
+      assertThat(date.toString(), is(
+              "Date: Oct 15, 2011, 12:00:00 AM GMT Other date: Oct 15, 2011, 1:05:15 AM GMT\nDate: Oct 6, 2011, 12:00:00 AM GMT\n"
+      ));
       date.set("test", D2);
-      assertEquals("Oct 6, 2011, 12:00:00 AM GMT", date.toString());
+      assertThat(date.toString(), is("Oct 6, 2011, 12:00:00 AM GMT"));
 
       date = date.get();
-      assertEquals("", date.toString());
+      assertThat(date.toString(), is(""));
       date.get("test").set("d1", D1).set("d2", D1_TIME).render();
       date.get("test").set("d1", D2).render();
-      assertEquals(
-              "Date: Oct 15, 2011, 12:00:00 AM GMT Other date: Oct 15, 2011, 1:05:15 AM GMT\nDate: Oct 6, 2011, 12:00:00 AM GMT\n",
-              date.toString()
-      );
+      assertThat(date.toString(), is(
+              "Date: Oct 15, 2011, 12:00:00 AM GMT Other date: Oct 15, 2011, 1:05:15 AM GMT\nDate: Oct 6, 2011, 12:00:00 AM GMT\n"
+      ));
     }
 
     @Test
@@ -79,21 +76,21 @@ class InheritanceTest {
       Template test1 = date.get("test");
       test1.set("d2", D1_TIME).render();
       date.set("d1", D1);
-      assertEquals("Date: 10/15/11, 12:00:00 AM Other date: 10/15/11, 1:05:15 AM", date.toString());
+      assertThat(date.toString(), is("Date: 10/15/11, 12:00:00 AM Other date: 10/15/11, 1:05:15 AM"));
       date.set("test", D2);
-      assertEquals("Date: 10/15/11, 12:00:00 AM10/6/11, 12:00:00 AM", date.toString());
+      assertThat(date.toString(), is("Date: 10/15/11, 12:00:00 AM10/6/11, 12:00:00 AM"));
 
       Template test2 = date.get("test");
-      assertEquals(" Other date: {v:d2}", test2.toString());
+      assertThat(test2.toString(), is(" Other date: {v:d2}"));
       test2.set("d2", D2);
-      assertEquals(" Other date: 10/15/11, 1:05:15 AM", test1.toString());
-      assertEquals(" Other date: 10/6/11, 12:00:00 AM", test2.toString());
+      assertThat(test1.toString(), is(" Other date: 10/15/11, 1:05:15 AM"));
+      assertThat(test2.toString(), is(" Other date: 10/6/11, 12:00:00 AM"));
 
       date = date.get();
-      assertEquals("", date.toString());
+      assertThat(date.toString(), is(""));
       date.set("d1", D2).get("test").set("d2", D2).render();
       date.set("d1", D1);
-      assertEquals("Date: 10/15/11, 12:00:00 AM Other date: 10/6/11, 12:00:00 AM", date.toString());
+      assertThat(date.toString(), is("Date: 10/15/11, 12:00:00 AM Other date: 10/6/11, 12:00:00 AM"));
     }
 
     @Test
@@ -105,108 +102,106 @@ class InheritanceTest {
       Template test1 = date.get("test");
       test1.set("d2", D1_TIME).render();
       date.set("d1", D1);
-      assertEquals("Date: 10/15/11, 12:00:00 AM Other date: 10/15/11, 1:05:15 AM", date.toString());
+      assertThat(date.toString(), is("Date: 10/15/11, 12:00:00 AM Other date: 10/15/11, 1:05:15 AM"));
       date.set("test", D2);
-      assertEquals("Date: 10/15/11, 12:00:00 AM10/6/11, 12:00:00 AM", date.toString());
+      assertThat(date.toString(), is("Date: 10/15/11, 12:00:00 AM10/6/11, 12:00:00 AM"));
 
       Template test2 = date.get("test");
-      assertEquals("", test2.toString());
+      assertThat(test2.toString(), is(""));
       test2.set("d2", D2);
-      assertEquals(" Other date: 10/15/11, 1:05:15 AM", test1.toString());
-      assertEquals(" Other date: 10/6/11, 12:00:00 AM", test2.toString());
+      assertThat(test1.toString(), is(" Other date: 10/15/11, 1:05:15 AM"));
+      assertThat(test2.toString(), is(" Other date: 10/6/11, 12:00:00 AM"));
 
       date = date.get();
-      assertEquals("", date.toString());
+      assertThat(date.toString(), is(""));
       date.set("d1", D2).get("test").set("d2", D2).render();
       date.set("d1", D1);
-      assertEquals("Date: 10/15/11, 12:00:00 AM Other date: 10/6/11, 12:00:00 AM", date.toString());
+      assertThat(date.toString(), is("Date: 10/15/11, 12:00:00 AM Other date: 10/6/11, 12:00:00 AM"));
     }
   }
 
-  @DisplayName("Inheritance.FromRCondTest")
-  static class FromRCond {
+  @Nested
+  class FromRCond {
     @Test
     void simple() {
       Template date = XML_ALIKE.parse("before<t: date='sql'>->{v:test}<-</t:>after");
-      assertEquals("beforeafter", date.toString());
+      assertThat(date.toString(), is("beforeafter"));
       date.set("test", D1);
-      assertEquals("before->2011-10-15<-after", date.toString());
+      assertThat(date.toString(), is("before->2011-10-15<-after"));
 
       date = date.get();
-      assertEquals("beforeafter", date.toString());
+      assertThat(date.toString(), is("beforeafter"));
       date.set("test", D1);
-      assertEquals("before->2011-10-15<-after", date.toString());
+      assertThat(date.toString(), is("before->2011-10-15<-after"));
     }
 
     @Test
     void nestedCond() {
       Template date = XML_ALIKE.parse("before<t: date='sql'>-><t:>{v:test}</t:><-</t:>after");
-      assertEquals("beforeafter", date.toString());
+      assertThat(date.toString(), is("beforeafter"));
       date.set("test", D1);
-      assertEquals("before->2011-10-15<-after", date.toString());
+      assertThat(date.toString(), is("before->2011-10-15<-after"));
 
       date = date.get();
-      assertEquals("beforeafter", date.toString());
+      assertThat(date.toString(), is("beforeafter"));
       date.set("test", D1);
-      assertEquals("before->2011-10-15<-after", date.toString());
+      assertThat(date.toString(), is("before->2011-10-15<-after"));
     }
 
     @Test
     void nestedRegion() {
       Template date = XML_ALIKE.parse("before<t: date='sql'>-><t:v1>{v:test}</t:v1><-</t:>after");
-      assertEquals("beforeafter", date.toString());
+      assertThat(date.toString(), is("beforeafter"));
       date.get("v1").set("test", D1).render();
-      assertEquals("before->2011-10-15<-after", date.toString());
+      assertThat(date.toString(), is("before->2011-10-15<-after"));
 
       Template v1 = date.get("v1");
-      assertEquals("{v:test}", v1.toString());
+      assertThat(v1.toString(), is("{v:test}"));
       v1.append("test", D2);
-      assertEquals("2011-10-06", v1.toString());
+      assertThat(v1.toString(), is("2011-10-06"));
 
       date = date.get();
-      assertEquals("beforeafter", date.toString());
+      assertThat(date.toString(), is("beforeafter"));
       date.get("v1").set("test", D1).render();
-      assertEquals("before->2011-10-15<-after", date.toString());
+      assertThat(date.toString(), is("before->2011-10-15<-after"));
     }
   }
 
-  @DisplayName("Inheritance.FromRegionTest")
-  static class FromRegion {
-    static {
-      TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
-    }
-
+  @Nested
+  class FromRegion {
     @Test
     void toRegion() {
       Template date = XML_ALIKE.parse(
-              "<t:test date='short'>Date 1: {v:d1}<t:test date='short'> Date 2: {v:d1}</t:test></t:test>").get("test");
+              "<t:test date='short'>Date 1: {v:d1}<t:test date='short'> Date 2: {v:d1}</t:test></t:test>"
+      ).get("test");
       date.set("d1", D1);
       date.get("test").set("d1", D2).render();
-      assertEquals("Date 1: 10/15/11 Date 2: 10/6/11", date.toString());
+      assertThat(date.toString(), is("Date 1: 10/15/11 Date 2: 10/6/11"));
 
     }
 
     @Test
     void toCond() {
       Template date = XML_ALIKE.parse(
-              "<t:test date=\"short_full\">Date 1: {v:d1 date='sql'}<t:> Date 2: {v:d2} </t:></t:test>", Locale.GERMAN)
-              .get("test");
+              "<t:test date=\"short_full\">Date 1: {v:d1 date='sql'}<t:> Date 2: {v:d2} </t:></t:test>",
+              Locale.GERMAN
+      ).get("test");
       date.set("d1", D1);
       date.set("d2", D2);
-      assertEquals("Date 1: 2011-10-15 Date 2: 06.10.11, 00:00:00 Mittlere Greenwich-Zeit ", date.toString());
-
+      assertThat(date.toString(), is("Date 1: 2011-10-15 Date 2: 06.10.11, 00:00:00 Mittlere Greenwich-Zeit "));
     }
 
     @Test
     void nestedRegion() {
       Template date = XML_ALIKE
               .read("<t:test date='_medium'><t:>Date 1: {v:d1}</t:><t:test><t:> Date 2: {v:d2} </t:></t:test></t:test>")
-              .locale(Locale.GERMAN).attrib("date", "long_short").parse().get("test");
+              .locale(Locale.GERMAN)
+              .attrib("date", "long_short")
+              .parse()
+              .get("test");
       date.set("d1", D1_TIME);
       date.get("test").set("d2", D2).render();
-      assertEquals("Date 1: 01:05:15 Date 2: 00:00:00 ", date.toString());
+      assertThat(date.toString(), is("Date 1: 01:05:15 Date 2: 00:00:00 "));
     }
-
   }
-
 }
