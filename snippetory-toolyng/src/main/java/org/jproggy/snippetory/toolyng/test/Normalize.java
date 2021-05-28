fea1eeecb0c0566836969e01eaefaaecc0e565aa
Bernd Ebertz <bebertz@jproggy.org>
2021-05-28 22:19:38 +0200

    Fix merge problems

--- snippetory-toolyng/src/main/java/org/jproggy/snippetory/toolyng/test/Normalize.java
+++ snippetory-toolyng/src/main/java/org/jproggy/snippetory/toolyng/test/Normalize.java
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-package org.jproggy.snippetory.toolyng.test;
-
-import java.util.regex.Pattern;
-
-import junit.framework.ComparisonFailure;
-
-public class Normalize {
-  private static final Pattern WS = Pattern.compile("[ \t\r\n]*");
-
-  static public void assertNormEquals(String message, String expected, String actual) {
-    if (!normalize(expected).equals(normalize(actual))) {
-      String cleanMessage= message == null ? "" : message;
-      throw new ComparisonFailure(cleanMessage, expected, actual);
-    }
-  }
-  public static void assertNormEquals(String expected, String actual) {
-    assertNormEquals(null, expected, actual);
-  }
-
-  private static String normalize(String val) {
-    if (val == null) return "";
-    return WS.matcher(val).replaceAll("");
-  }
-}
-=======
 package org.jproggy.snippetory.toolyng.test;
 
 import junit.framework.ComparisonFailure;
@@ -49,4 +23,3 @@ public class Normalize {
     return WS.matcher(val).replaceAll("");
   }
 }
->>>>>>> master
--- snippetory-toolyng/src/main/java/org/jproggy/snippetory/toolyng/test/TestRecorderTpl.java
+++ snippetory-toolyng/src/main/java/org/jproggy/snippetory/toolyng/test/TestRecorderTpl.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.jproggy.snippetory.toolyng.test;
 
 import java.util.ArrayList;
@@ -26,32 +25,3 @@ public class TestRecorderTpl {
 	}
 	// region }
 }
-=======
-package org.jproggy.snippetory.toolyng.test;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-
-public class TestRecorderTpl {
-	// ${ region
-	public Map<String, Object> getData() {/*${name backward="Data" case="camelizeUpper"}*/
-		List<Object> values;
-		Map<String, Object> data = new HashMap<String, Object>();
-		// ${ set
-		data.put("${name enc='string'}", /*${data*/10/*}*/); 
-		// set }
-		// ${ append
-		values = new ArrayList<Object>();
-		// ${ value
-		values.add(/*${data*/10/*}*/);
-		// value }
-		data.put("${name enc='string'}", /*${data*/values/*}*/); 
-		// append }
-		return data;
-	}
-	// region }
-}
->>>>>>> master
